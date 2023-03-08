f260cd8fbf8c085bce0266ebc851979156dd9238
William <william@underage.wang>
2023-03-08 09:24:59 +0100

    change default font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono for Powerline:pixelsize=35:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:size=25";
 static int borderpx = 2;
 
 /*
